e9990a8a6696c51a56b292752ccf0269683a4d9e
tanukaaa <tanuka@mail.bg>
2016-11-29 19:34:07 +0200

    Created the backend 'ArticleController' logic for the view

--- src/main/java/softuni/blog/controller/ArticleController.java
+++ src/main/java/softuni/blog/controller/ArticleController.java
@@ -1,7 +1,10 @@
 package softuni.blog.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import softuni.blog.repository.ArticleRepository;
 import softuni.blog.repository.UserRepository;
 
@@ -23,5 +26,23 @@ public class ArticleController {
     private UserRepository userRepository;
 
 
-    
+
+
+
+    //The "@GetMapping" annotation tells Spring that this method cannot be called if the user wants to submit data.
+    // It should be only used for viewing data, in our case showing the form.
+    // The "@PreAuthorize" annotation uses Spring Security. That annotation receives a parameter,
+    // which tell the authentication module who can access our method.
+    // We want to limit the article creation to logged in users only and that’s why we are using the "isAuthenticated()" parameter.
+    @GetMapping("/article/create")
+    @PreAuthorize("isAuthenticated()")
+    public String create(Model model){
+
+        //This code will add to our model a key-value pair.
+        //The key will be the view we want to render and the value is the path to our view.
+        // We want to load the "create" file from the "article" folder. Then we simply tell Spring to use our base layout.
+        model.addAttribute("view", "article/create");
+        return "base-layout";
+    }
+
 }
