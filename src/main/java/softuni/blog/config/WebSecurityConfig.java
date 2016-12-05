4de556ef514cf49e32280952d09a8bce5be8b5aa
tanukaaa <tanuka@mail.bg>
2016-12-05 16:38:27 +0200

    We added Access Control because andyone could access /users/delete/'id'. Now if someone tries to access it he/she gets redirected to an error page.

--- src/main/java/softuni/blog/config/WebSecurityConfig.java
+++ src/main/java/softuni/blog/config/WebSecurityConfig.java
@@ -45,6 +45,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http)throws Exception{
 
         http.authorizeRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest()
                 .permitAll().and()
                 .formLogin().loginPage("/login")
--- src/main/java/softuni/blog/controller/HomeController.java
+++ src/main/java/softuni/blog/controller/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import softuni.blog.entity.Article;
 import softuni.blog.repository.ArticleRepository;
 
@@ -30,6 +31,15 @@ public class HomeController {
 
         model.addAttribute("view", "home/index");
         model.addAttribute("articles", articles);
+        return "base-layout";
+    }
+
+
+    @RequestMapping("/error/403")
+    public String accessDenied(Model model){
+
+        model.addAttribute("view", "error/403");
+
         return "base-layout";
     }
 }
