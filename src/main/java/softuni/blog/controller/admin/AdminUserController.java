e2b4b8c0ee3b698806ae2bb1e631a65af7e60820
tanukaaa <tanuka@mail.bg>
2016-12-05 15:09:54 +0200

    Implemented the 'edit' button in the admin users/list pannel now the admins can add different roles change names etc

--- src/main/java/softuni/blog/controller/admin/AdminUserController.java
+++ src/main/java/softuni/blog/controller/admin/AdminUserController.java
@@ -1,16 +1,24 @@
 package softuni.blog.controller.admin;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import softuni.blog.bindingModel.UserEditBindingModel;
+import softuni.blog.entity.Role;
 import softuni.blog.entity.User;
 import softuni.blog.repository.ArticleRepository;
 import softuni.blog.repository.RoleRepository;
 import softuni.blog.repository.UserRepository;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by Admin on 5.12.2016 г..
@@ -46,5 +54,75 @@ public class AdminUserController {
         model.addAttribute("view", "admin/user/list");
 
         return "base-layout";
+    }
+
+
+
+    //GET edit
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model){
+
+        //Some baseCase
+        if(!this.userRepository.exists(id)){
+            return "redirect:/admin/users/";
+        }
+
+        //We are getting all of the roles from the database and sending them to the view
+        //Then we are sending the user we are going to edit to the view as well
+
+        User user = this.userRepository.findOne(id);
+        List<Role> roles = this.roleRepository.findAll();
+
+        model.addAttribute("user", user);
+        model.addAttribute("roles", roles);
+        model.addAttribute("view", "admin/user/edit");
+
+        return "base-layout";
+    }
+
+
+    //POST edit
+    @PostMapping("/edit/{id}")
+    public String editProcess(@PathVariable Integer id, UserEditBindingModel userEditBindingModel){
+
+        //baseCase
+        if(!this.userRepository.exists(id)){
+            return "redirect:/admin/users/";
+        }
+
+        User user = this.userRepository.findOne(id);
+
+        //We don't want to change the user password if the admin left the password fields empty
+        if(!StringUtils.isEmpty(userEditBindingModel.getPassword()) && !StringUtils.isEmpty(userEditBindingModel.getConfirmPassword())){
+
+            if(userEditBindingModel.getPassword().equals(userEditBindingModel.getConfirmPassword())){
+                BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+                user.setPassword(bCryptPasswordEncoder.encode(userEditBindingModel.getPassword()));
+            }
+
+            // In the first if-statement we check if the password fields are not empty.
+            // Then, in the second if-statement we check if the two passwords match.
+            // If the match, we encrypt the password and set it as a new password for our user.
+        }
+        // end
+
+
+        //Editing the full name and/or Email
+        user.setFullName(userEditBindingModel.getFullName());
+        user.setEmail(userEditBindingModel.getEmail());
+
+        Set<Role> roles = new HashSet<>();
+
+        for(Integer roleId : userEditBindingModel.getRoles()){
+            roles.add(this.roleRepository.findOne(id));
+        }
+
+        user.setRoles(roles);
+
+        this.userRepository.saveAndFlush(user);
+
+        return "redirect:/admin/users/";
+
     }
 }
